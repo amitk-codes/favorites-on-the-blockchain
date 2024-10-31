ec2a466b7df21b1be7ed673e2604709305dc2dbe
Amit Kumar <amitkcodes@gmail.com>
2024-10-31 12:03:58 +0530

    added max length macro to color and hobbies property of Favorites

--- programs/favorites_on_the_blockchain/src/lib.rs
+++ programs/favorites_on_the_blockchain/src/lib.rs
@@ -11,6 +11,10 @@ pub mod favorites_on_the_blockchain {
 #[derive(InitSpace)]
 pub struct Favorites{
     pub number: u64,
+
+    #[max_len(60)]
     pub color: String,
+
+    #[max_len(5, 60)]
     pub hobbies: Vec<String>,
 }
