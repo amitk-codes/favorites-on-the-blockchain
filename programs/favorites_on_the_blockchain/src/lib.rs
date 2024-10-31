2967dea5acbc416fe81814b5cc1b0cbbcbb4fedc
Amit Kumar <amitkcodes@gmail.com>
2024-10-31 11:52:54 +0530

    created a struct for Favorites account

--- programs/favorites_on_the_blockchain/src/lib.rs
+++ programs/favorites_on_the_blockchain/src/lib.rs
@@ -5,12 +5,12 @@ declare_id!("8Lw8tQWgYc64CCRxA9w4K36iTkhUYKpuHe56hU4wRTHi");
 #[program]
 pub mod favorites_on_the_blockchain {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites{
+    pub number: u64,
+    pub color: String,
+    pub hobbies: Vec<String>,
+}
