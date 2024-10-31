3b9fee66c70ee5c6b675ecf54ed4fbf8c081d51c
Amit Kumar <amitkcodes@gmail.com>
2024-10-31 14:39:44 +0530

    added a instruction handler function for setting favorites

--- programs/favorites_on_the_blockchain/src/lib.rs
+++ programs/favorites_on_the_blockchain/src/lib.rs
@@ -5,6 +5,33 @@ declare_id!("8Lw8tQWgYc64CCRxA9w4K36iTkhUYKpuHe56hU4wRTHi");
 #[program]
 pub mod favorites_on_the_blockchain {
     use super::*;
+
+    pub fn set_favorites(
+        ctx: Context<SetFavorites>,
+        number: u64,
+        color: String,
+        hobbies: Vec<String>,
+    ) -> Result<()> {
+        msg!("Calling 'set_favorites' function with program_id: {}", ctx.program_id);
+        
+        let color_clone = color.clone();
+        let hobbies_clone = hobbies.clone();
+        
+        ctx.accounts.favorites.set_inner(Favorites {
+            number,
+            color,
+            hobbies,
+        });
+
+        msg!("For user with public key {}, favorite number is set to {}, color is set to {}, and hobbies are set to {:?}", 
+            ctx.accounts.user.key(), 
+            number, 
+            color_clone, 
+            hobbies_clone
+        );
+
+        Ok(())
+    }
 }
 
 #[account]
@@ -25,7 +52,7 @@ pub struct SetFavorites<'info> {
     pub user: Signer<'info>,
 
     #[account(
-        mut,
+        init_if_needed,
         payer = user,
         space = 8 + Favorites::INIT_SPACE,
         seeds = [b"favorites", user.key().as_ref()],
